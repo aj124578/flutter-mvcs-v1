e0872753fe96cdf2523dde2b4aed448bc97884d1
aj124578 <aj124578@naver.com>
2023-04-18 11:19:12 +0900

    logout, auto login

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_blog_start/core/constants/http.dart';
 import 'package:flutter_riverpod_blog_start/core/constants/move.dart';
-import 'package:flutter_riverpod_blog_start/dto/response_dto.dart';
 import 'package:flutter_riverpod_blog_start/model/user/user_repository.dart';
 import 'package:flutter_riverpod_blog_start/provider/session_provider.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-Future<SessionUser> appLoading() async{
-  ResponseDTO responseDTO = await UserRepository().fetchJwtVerify();
-  SessionUser sessionUser = SessionUser();
-  if(responseDTO.code == 1){
-    sessionUser.loginSuccess(responseDTO.data, responseDTO.token!);
-  }else{
-    sessionUser.logoutSuccess();
-  }
-  return sessionUser;
-}
+
 
 void main() async{
   // MyApp 시작전에 필요한 것 여기서 다 로딩 (동기적 실행)
@@ -25,7 +14,7 @@ void main() async{
   // 1. 시큐어 스토리지에 JWT 있는 확인
   // 2. JWT를 가지고 회원정보를 가져와서!!
   // 3. SessionUser 동기화 (ref에 접근해야함)
-  SessionUser sessionUser = await appLoading();
+  SessionUser sessionUser = await UserRepository().fetchJwtVerify();
 
   runApp(
     ProviderScope(
--- lib/model/user/user_repository.dart
+++ lib/model/user/user_repository.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod_blog_start/dto/user_request.dart';
 import 'package:flutter_riverpod_blog_start/dto/response_dto.dart';
 import 'package:logger/logger.dart';
 
+import '../../provider/session_provider.dart';
 import 'user.dart';
 
 class UserRepository {
@@ -14,11 +15,9 @@ class UserRepository {
   }
   UserRepository._single();
 
-  Future<ResponseDTO> fetchJwtVerify() async {
+  Future<SessionUser> fetchJwtVerify() async {
+    SessionUser sessionUser = SessionUser();
     String? deviceJwt = await secureStorage.read(key: "jwt");
-    Logger().d("토큰 : "+deviceJwt!);
-
-    // deviceJwt 를 base64로 디코딩 - payload값에 id를 확인해서 요청
     if(deviceJwt != null){
       try{
         Response response = await dio.get("/jwtToken", options: Options(
@@ -29,16 +28,23 @@ class UserRepository {
         ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
         responseDTO.token = deviceJwt;
         responseDTO.data = User.fromJson(responseDTO.data);
-        return responseDTO;
+
+        if(responseDTO.code == 1){
+          sessionUser.loginSuccess(responseDTO.data, responseDTO.token!);
+        }else{
+          sessionUser.logoutSuccess();
+        }
+        return sessionUser;
       }catch(e){
         Logger().d("에러 이유 : "+e.toString());
-        return ResponseDTO(code: -1, msg: "jwt토큰이 유효하지 않습니다");
+        sessionUser.logoutSuccess();
+        return sessionUser;
       }
     }else{
-      return ResponseDTO(code: -1, msg: "jwt토큰이 존재하지 않습니다");
+      sessionUser.logoutSuccess();
+      return sessionUser;
     }
   }
-
   Future<ResponseDTO> fetchJoin(JoinReqDTO joinReqDTO) async {
     try{
       Response response = await dio.post("/join", data: joinReqDTO.toJson());
@@ -49,20 +55,23 @@ class UserRepository {
       return ResponseDTO(code: -1, msg: "유저네임중복");
     }
   }
-
   Future<ResponseDTO> fetchLogin(LoginReqDTO loginReqDTO) async {
-    // 1. 통신 시작
-    Response response = await dio.post("/login", data: loginReqDTO.toJson());
+    try{
+      // 1. 통신 시작
+      Response response = await dio.post("/login", data: loginReqDTO.toJson());
 
-    // 2. DTO 파싱
-    ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
-    responseDTO.data = User.fromJson(responseDTO.data);
+      // 2. DTO 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      responseDTO.data = User.fromJson(responseDTO.data);
 
-    // 3. 토큰 받기
-    final authorization = response.headers["authorization"];
-    if(authorization != null){
-      responseDTO.token = authorization.first;
+      // 3. 토큰 받기
+      final authorization = response.headers["authorization"];
+      if(authorization != null){
+        responseDTO.token = authorization.first;
+      }
+      return responseDTO;
+    }catch(e){
+      return ResponseDTO(code: -1, msg: "유저네임 혹은 비번이 틀렸습니다");
     }
-    return responseDTO;
   }
 }
